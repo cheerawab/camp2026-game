8b3e7bf89631ee112750daf5b53bb5105e6d3bb0
noeFly <noeFly@proton.me>
2026-06-06 15:38:17 +0800

    style: format with prettier

--- frontend/src/routes/battle/index.tsx
+++ frontend/src/routes/battle/index.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router"
 
-export const Route = createFileRoute('/battle/')({
+export const Route = createFileRoute("/battle/")({
   component: RouteComponent,
 })
 
--- frontend/src/routes/battle/ingame.tsx
+++ frontend/src/routes/battle/ingame.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router"
 
-export const Route = createFileRoute('/battle/ingame')({
+export const Route = createFileRoute("/battle/ingame")({
   component: RouteComponent,
 })
 
