acde87f8318ad8ec156ff3fdf91b3a5e28fc3935
Yorukot <hi@yorukot.me>
2026-06-06 15:59:52 +0800

    Remove component gallery page

--- frontend/src/routes/component-gallery.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router"
-
-import { ComponentsPage } from "@/pages/components/ui/components-page"
-
-export const Route = createFileRoute("/component-gallery")({
-  component: ComponentsPage,
-})
