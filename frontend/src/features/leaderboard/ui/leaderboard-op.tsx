33e2505e2bcc9f2985a1cd0f44e26d1dd90efde5
noeFly <noeFly@proton.me>
2026-06-07 16:51:53 +0800

    feat: add LeaderboardPage

---
+++ frontend/src/features/leaderboard/ui/leaderboard-op.tsx
@@ -0,0 +1,57 @@
+import { Card, CardContent } from "@/shared/ui/card"
+
+const data = [
+  {
+    teamName: "A",
+    pictureSrc: "https://placehold.co/100x100/svg",
+    score: '100'
+  },
+  {
+    teamName: "B",
+    pictureSrc: "https://placehold.co/100x100/svg",
+    score: '80'
+  },
+  {
+    teamName: "C",
+    pictureSrc: "https://placehold.co/100x100/svg",
+    score: '70'
+  },
+  {
+    teamName: "A",
+    pictureSrc: "https://placehold.co/100x100/svg",
+    score: '60'
+  },
+  {
+    teamName: "A",
+    pictureSrc: "https://placehold.co/100x100/svg",
+    score: '50'
+  },
+  {
+    teamName: "A",
+    pictureSrc: "https://placehold.co/100x100/svg",
+    score: '40'
+  },
+]
+
+const highlight = 2
+
+export function LeaderboardOp() {
+  return (
+    <div className="grid gap-y-2">
+      {data.map((item, index) => {
+        return (
+          <Card className={index + 1 === highlight ? "bg-secondary font-bold" : ""}>
+            <CardContent className="flex items-center gap-x-4">
+              <span>#{index + 1}</span>
+              <img src={item.pictureSrc} className="h-12" />
+              <div className="flex flex-1 justify-between">
+                <span>{item.teamName} 隊</span>
+                <span>{item.score} OP</span>
+              </div>
+            </CardContent>
+          </Card>
+        )
+      })}
+    </div>
+  )
+}
---
+++ frontend/src/pages/leaderboard/ui/leadboard-page.tsx
@@ -0,0 +1,29 @@
+import { LeaderboardBattle } from "@/features/leaderboard/ui/leaderboard-battle"
+import { LeaderboardOp } from "@/features/leaderboard/ui/leaderboard-op"
+import { LeaderboardSitone } from "@/features/leaderboard/ui/leaderboard-sitone"
+import { PageHeader } from "@/shared/ui/page-header"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/shared/ui/tabs"
+
+export function LeaderboardPage() {
+  return (
+    <main className="mx-auto w-full max-w-sm gap-y-2 py-4">
+      <PageHeader title="小隊排行榜" headline="Leaderboard" />
+      <Tabs defaultValue="op">
+        <TabsList className="mx-auto">
+          <TabsTrigger value="op">開源力</TabsTrigger>
+          <TabsTrigger value="sitone">小石</TabsTrigger>
+          <TabsTrigger value="battle">知識王戰</TabsTrigger>
+        </TabsList>
+        <TabsContent value="op">
+          <LeaderboardOp />
+        </TabsContent>
+        <TabsContent value="sitone">
+          <LeaderboardSitone />
+        </TabsContent>
+        <TabsContent value="battle">
+          <LeaderboardBattle />
+        </TabsContent>
+      </Tabs>
+    </main>
+  )
+}
