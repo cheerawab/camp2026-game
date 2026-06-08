802f2aacb2c033c920a9a353164d495d5cb4ee21
noeFly <noeFly@proton.me>
2026-06-08 15:12:53 +0800

    feat: add team info section

--- frontend/src/pages/leaderboard/ui/leadboard-page.tsx
+++ frontend/src/pages/leaderboard/ui/leadboard-page.tsx
@@ -1,12 +1,33 @@
 import { LeaderboardOp } from "@/features/leaderboard/ui/leaderboard-op"
 import { LeaderboardSitone } from "@/features/leaderboard/ui/leaderboard-sitone"
+import { Card, CardContent } from "@/shared/ui/card"
 import { PageHeader } from "@/shared/ui/page-header"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/shared/ui/tabs"
 
 export function LeaderboardPage() {
   return (
-    <main className="mx-auto w-full max-w-sm gap-y-2 py-4">
+    <main className="mx-auto grid w-full max-w-sm gap-y-2 py-4">
       <PageHeader title="小隊排行榜" headline="Leaderboard" />
+      {/* 小隊資訊 */}
+      <Card className="bg-accent text-accent-foreground">
+        <CardContent className="flex">
+          <div className="grid basis-3/4 gap-y-2">
+            <span className="text-muted-foreground text-sm font-bold">
+              你的隊伍
+            </span>
+            <span className="text-2xl font-bold">B 小隊目前排名第二！</span>
+            <span className="text-wrap wrap-break-word">
+              距離第一名的差距不大，僅僅 20 OP，何不嘗試拼一下呢？
+            </span>
+          </div>
+          <div className="basis-1/4 flex items-center justify-center">
+            <span className="border-foreground bg-secondary rotate-3 rounded-lg border-2 p-5 text-2xl font-bold">
+              #2
+            </span>
+          </div>
+        </CardContent>
+      </Card>
+      {/* 完整排行榜 */}
       <Tabs defaultValue="op">
         <TabsList className="w-full">
           <TabsTrigger value="op">開源力</TabsTrigger>
