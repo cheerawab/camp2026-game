8a3cae0979e0a3308ed2f5974f1ea1716f8c9250
noeFly <noeFly@proton.me>
2026-06-08 13:28:08 +0800

    feat: redesign group card layout

--- frontend/src/features/leaderboard/ui/leaderboard-op.tsx
+++ frontend/src/features/leaderboard/ui/leaderboard-op.tsx
@@ -1,34 +1,42 @@
 import { Card, CardContent } from "@/shared/ui/card"
+import { Separator } from "@/shared/ui/separator"
+import { cn } from "@/shared/utils"
 
 const data = [
   {
     teamName: "A",
-    pictureSrc: "https://placehold.co/100x100/svg",
+    pictureSrc: "https://placehold.co/100x100/orange/gray/?text=Group+Icon",
+    backgroundSrc: "https://placehold.co/300x50/white/gray/?text=Background",
     score: "100",
   },
   {
     teamName: "B",
-    pictureSrc: "https://placehold.co/100x100/svg",
+    pictureSrc: "https://placehold.co/100x100/orange/gray/?text=Group+Icon",
+    backgroundSrc: "https://placehold.co/300x50/white/gray/?text=Background",
     score: "80",
   },
   {
     teamName: "C",
-    pictureSrc: "https://placehold.co/100x100/svg",
+    pictureSrc: "https://placehold.co/100x100/orange/gray/?text=Group+Icon",
+    backgroundSrc: "https://placehold.co/300x50/white/gray/?text=Background",
     score: "70",
   },
   {
     teamName: "A",
-    pictureSrc: "https://placehold.co/100x100/svg",
+    pictureSrc: "https://placehold.co/100x100/orange/gray/?text=Group+Icon",
+    backgroundSrc: "https://placehold.co/300x50/white/gray/?text=Background",
     score: "60",
   },
   {
     teamName: "A",
-    pictureSrc: "https://placehold.co/100x100/svg",
+    pictureSrc: "https://placehold.co/100x100/orange/gray/?text=Group+Icon",
+    backgroundSrc: "https://placehold.co/300x50/white/gray/?text=Background",
     score: "50",
   },
   {
     teamName: "A",
-    pictureSrc: "https://placehold.co/100x100/svg",
+    pictureSrc: "https://placehold.co/100x100/orange/gray/?text=Group+Icon",
+    backgroundSrc: "https://placehold.co/300x50/white/gray/?text=Background",
     score: "40",
   },
 ]
@@ -39,18 +47,34 @@ export function LeaderboardOp() {
   return (
     <div className="grid gap-y-2">
       {data.map((item, index) => {
+        const isHighlight: boolean = index + 1 === highlight
         return (
           <Card
-            className={index + 1 === highlight ? "bg-secondary font-bold" : ""}
+            className={cn(
+              "flex flex-row items-center gap-x-0 overflow-clip py-0 text-lg",
+              isHighlight ? "bg-secondary font-bold" : "",
+            )}
           >
-            <CardContent className="flex items-center gap-x-4">
-              <span>#{index + 1}</span>
-              <img src={item.pictureSrc} className="h-12" />
-              <div className="flex flex-1 justify-between">
+            <span className="p-4">#{index + 1}</span>
+            <Separator
+              orientation="vertical"
+              className={isHighlight ? "bg-accent-foreground" : ""}
+            />
+            <div className="relative flex h-full w-full items-center gap-x-4 p-4">
+              {/* z-10 隊伍背景裝飾布局 */}
+              <img
+                src={item.backgroundSrc}
+                className="absolute top-0 left-0 z-10 h-full w-full object-cover"
+              />
+              {/* z-20 半透明圖層（增加前景文字對比） */}
+              <div className="absolute top-0 left-0 h-full w-full z-20 bg-background/50" />
+              {/* z-30 隊伍各項資訊 */}
+              <img src={item.pictureSrc} className="z-30 h-14 rounded-lg" />
+              <div className="z-20 flex flex-1 justify-between">
                 <span>{item.teamName} 隊</span>
                 <span>{item.score} OP</span>
               </div>
-            </CardContent>
+            </div>
           </Card>
         )
       })}
